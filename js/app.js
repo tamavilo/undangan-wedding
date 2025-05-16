c3216c4b9c3d904b9e6c8cad23df2cb7d241f4de
tamavilo <59593584+tamavilo@users.noreply.github.com>
2025-05-16 20:27:50 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -161,6 +161,7 @@ const util = (() => {
         div.innerHTML = `<p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p><h2 class="text-light">${escapeHtml(name)}</h2><p class="text-light" style="font-size: 0.9rem;font-weight:italic">*Mohon maaf bila ada kesalahan penulisan nama dan gelar</p>`;
 
         document.getElementById('form-nama').value = name;
+        document.getElementById('form-nama2').value = name;
         document.getElementById('nama-tamu').appendChild(div);
     };
 
